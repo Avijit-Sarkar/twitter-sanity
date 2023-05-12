289bc3279bcf9dda4cc8f3cc664ee1d57998f51d
Avijit-Sarkar <103516653+Avijit-Sarkar@users.noreply.github.com>
2023-05-12 18:21:06 +0530

    created the schemas

--- sanity/sanity.cli.js
+++ sanity/sanity.cli.js
@@ -3,6 +3,6 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'pu080y2f',
-    dataset: 'production'
-  }
+    dataset: 'production',
+  },
 })
