b85518ec501e4f2e5a6351762dd54699c2e86ed0
Avijit-Sarkar <103516653+Avijit-Sarkar@users.noreply.github.com>
2023-05-13 20:00:30 +0530

    Added the login and logout functionality

--- components/TweetBox.tsx
+++ components/TweetBox.tsx
@@ -7,14 +7,16 @@ import {
   PhotoIcon,
 } from "@heroicons/react/24/outline";
 import React, { useState } from "react";
+import { useSession } from "next-auth/react";
 
 function TweetBox() {
   const [input, SetInput] = useState<string>("");
+  const { data: session } = useSession();
 
   return (
     <div className="flex space-x-2 p-5">
       <img
-        src="https://links.papareact.com/gll"
+        src={session?.user?.image || "https://links.papareact.com/gll"}
         alt="image"
         className="mt-4 h-14 w-14 rounded-full object-cover"
       />
@@ -37,7 +39,7 @@ function TweetBox() {
               <MapPinIcon className="h-5 w-5" />
             </div>
             <button
-              disabled={!input}
+              disabled={!input || !session}
               className="rounded-full bg-twitter px-5 py-2 font-bold disabled:opacity-40 text-white"
             >
               Tweet
